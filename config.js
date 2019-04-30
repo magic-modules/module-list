08f33509bf8ae3b718435870c7396c0d9d592738
Jascha <jascha@jaeh.at>
2019-04-30 17:18:47 +0200

    add docs

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ROOT: 'example',
+  WEB_ROOT: '/module-list/',
+  PUBLIC: 'docs',
+  THEME: 'docs',
+}
---
+++ example/pages/index.js
@@ -0,0 +1,46 @@
+module.exports = () => [
+  h1('@magic-modules/module-list'),
+  p([
+    'this is the ',
+    Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
+    ' ModuleList component. It shows a list of all @magic-modules.',
+  ]),
+
+  GitBadges({
+    project: 'magic-modules/module-list',
+    appveyor: 'jaeh/module-list',
+  }),
+
+  h2({ id: 'installation' }, 'installation'),
+  Pre(`
+npm install magic-modules/module-list
+`),
+
+  h2({ id: 'require' }, 'require'),
+  p('first add the component to the assets'),
+  Pre(`
+// assets/index.js
+module.exports = {
+//...other exports
+ModuleList: require('@magic-modules/module-list'),
+}`),
+
+  h2({ id: 'usage' }, 'usage'),
+  p('in a page or component'),
+  Pre('module.exports = {\n  View: () => ModuleList,\n}'),
+
+  h2({ id: 'demo' }, 'demo'),
+  p('this is what it looks like'),
+  ModuleList,
+
+  h2({ id: 'source' }, 'source'),
+  p([
+    'the source for this page is in the ',
+    Link(
+      { to: 'https://github.com/magic-modules/module-list/tree/master/example' },
+      'example directory',
+    ),
+    ' and gets built and published to github using ',
+    Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
+  ]),
+]
