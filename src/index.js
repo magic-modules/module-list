b365850c0c038d64dfa53546393bb7dcfd08109f
Jascha <jascha@jaeh.at>
2019-04-30 17:35:06 +0200

    change list format

--- src/index.js
+++ src/index.js
@@ -1,48 +1,60 @@
-module.exports = () =>
-  ul([
+const ModuleList = () =>
+  ul({ class: 'ModuleList' }, [
     li([
-      h3('ModuleList'),
-      div(['the ModuleList module shows the list of @magic-modules you are looking at.']),
-      p([
+      h3(
+        { id: 'module-module-list' },
         Link({ to: 'https://github.com/magic-modules/module-list' }, '@magic-modules/module-list'),
-      ]),
-      p([Link({ to: 'https://magic-modules.github.io/module-list' }, 'docs / demo')]),
+      ),
+      div(['the ModuleList module shows the list of @magic-modules you are looking at.']),
+      Link({ to: 'https://magic-modules.github.io/module-list' }, 'docs / demo'),
     ]),
     li([
-      h3('Pre'),
-      div([
-        'the Pre module allows you to display javascript code with syntax highlighting.',
-        ' it is used throughout this documentation.',
-      ]),
-      p([Link({ to: 'https://github.com/magic-modules/pre' }, '@magic-modules/pre')]),
-      p([Link({ to: 'https://magic-modules.github.io/pre' }, 'docs / demo')]),
+      h3(
+        { id: 'module-pre' },
+        Link({ to: 'https://github.com/magic-modules/pre' }, '@magic-modules/pre'),
+      ),
+      div('the Pre module allows you to display javascript code with syntax highlighting.'),
+      Link({ to: 'https://magic-modules.github.io/pre' }, 'docs / demo'),
     ]),
     li([
-      h3('LanguageSwitch'),
-      div('file hierarchy based multilanguage support for magic.'),
-      p([
+      h3(
+        { id: 'module-language-switch' },
         Link(
           { to: 'https://github.com/magic-modules/languageswitch' },
           '@magic-modules/languageswitch',
         ),
-      ]),
-      p([Link({ to: 'https://magic-modules.github.io/languageswitch' }, 'docs / demo')]),
+      ),
+      div('file hierarchy based multilanguage support for magic.'),
+      Link({ to: 'https://magic-modules.github.io/languageswitch' }, 'docs / demo'),
     ]),
     li([
-      h3('VideoEmbed'),
+      h3(
+        { id: 'module-video-embed' },
+        Link({ to: 'https://github.com/magic-modules/video-embed' }, '@magic-modules/video-embed'),
+      ),
       div([
         'the VideoEmbed module embeds videos from any video hoster using iframes. ',
         ' comes with default support for vimeo and youtube.',
       ]),
-      p([
-        Link({ to: 'https://github.com/magic-modules/video-embed' }, '@magic-modules/video-embed'),
-      ]),
-      p([Link({ to: 'https://magic-modules.github.io/video-embed' }, 'docs / demo')]),
+      Link({ to: 'https://magic-modules.github.io/video-embed' }, 'docs / demo'),
     ]),
     li([
-      h3('GitBadges'),
+      h3(
+        { id: 'module-git-badges' },
+        Link({ to: 'https://github.com/magic-modules/gitbadges' }, '@magic-modules/gitbadges'),
+      ),
       div('shows a list of github repository status badges.'),
-      p([Link({ to: 'https://github.com/magic-modules/gitbadges' }, '@magic-modules/gitbadgesi')]),
-      p([Link({ to: 'https://magic-modules.github.io/gitbadges' }, 'docs / demo')]),
+      Link({ to: 'https://magic-modules.github.io/gitbadges' }, 'docs / demo'),
     ]),
   ])
+
+ModuleList.style = {
+  '.ModuleList': {
+    a: {
+      display: 'block',
+      lineHeight: 1.8,
+    },
+  },
+}
+
+module.exports = ModuleList
