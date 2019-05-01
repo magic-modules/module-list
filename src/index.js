0aaa8d8f6139fc0703a1d7f5c54d04457b29baad
Jascha <jascha@jaeh.at>
2019-05-01 17:44:46 +0200

    use gitbadges

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const ModuleList = () =>
         Link({ to: 'https://github.com/magic-modules/module-list' }, '@magic-modules/module-list'),
       ),
       div(['the ModuleList module shows the list of @magic-modules you are looking at.']),
+      GitBadges({
+        project: 'magic-modules/module-list',
+        appveyor: 'jaeh/module-list',
+      }),
       Link({ to: 'https://magic-modules.github.io/module-list' }, 'docs / demo'),
     ]),
     li([
@@ -14,6 +18,10 @@ const ModuleList = () =>
         Link({ to: 'https://github.com/magic-modules/theme-list' }, '@magic-modules/theme-list'),
       ),
       div(['the ThemeList module shows a list of all @magic-themes.']),
+      GitBadges({
+        project: 'magic-modules/theme-list',
+        appveyor: 'jaeh/theme-list',
+      }),
       Link({ to: 'https://magic-modules.github.io/theme-list' }, 'docs / demo'),
     ]),
     li([
@@ -22,6 +30,10 @@ const ModuleList = () =>
         Link({ to: 'https://github.com/magic-modules/client-list' }, '@magic-modules/client-list'),
       ),
       div(['the ClientList module shows a list of all @magic-client libraries.']),
+      GitBadges({
+        project: 'magic-modules/client-list',
+        appveyor: 'jaeh/client-list',
+      }),
       Link({ to: 'https://magic-modules.github.io/client-list' }, 'docs / demo'),
     ]),
     li([
@@ -30,6 +42,10 @@ const ModuleList = () =>
         Link({ to: 'https://github.com/magic-modules/pre' }, '@magic-modules/pre'),
       ),
       div('the Pre module allows you to display javascript code with syntax highlighting.'),
+      GitBadges({
+        project: 'magic-modules/pre',
+        appveyor: 'jaeh/pre',
+      }),
       Link({ to: 'https://magic-modules.github.io/pre' }, 'docs / demo'),
     ]),
     li([
@@ -41,6 +57,10 @@ const ModuleList = () =>
         ),
       ),
       div('file hierarchy based multilanguage support for magic.'),
+      GitBadges({
+        project: 'magic-modules/language-switch',
+        appveyor: 'jaeh/language-switch',
+      }),
       Link({ to: 'https://magic-modules.github.io/languageswitch' }, 'docs / demo'),
     ]),
     li([
@@ -52,6 +72,10 @@ const ModuleList = () =>
         'the VideoEmbed module embeds videos from any video hoster using iframes. ',
         ' comes with default support for vimeo and youtube.',
       ]),
+      GitBadges({
+        project: 'magic-modules/video-embed',
+        appveyor: 'jaeh/video-embed',
+      }),
       Link({ to: 'https://magic-modules.github.io/video-embed' }, 'docs / demo'),
     ]),
     li([
@@ -60,6 +84,10 @@ const ModuleList = () =>
         Link({ to: 'https://github.com/magic-modules/gitbadges' }, '@magic-modules/gitbadges'),
       ),
       div('shows a list of github repository status badges.'),
+      GitBadges({
+        project: 'magic-modules/gitbadges',
+        appveyor: 'jaeh/gitbadges',
+      }),
       Link({ to: 'https://magic-modules.github.io/gitbadges' }, 'docs / demo'),
     ]),
   ])
@@ -71,6 +99,10 @@ ModuleList.style = {
       lineHeight: 1.8,
     },
   },
+}
+
+ModuleList.dependencies = {
+  GitBadges: require('@magic-modules/gitbadges'),
 }
 
 module.exports = ModuleList
