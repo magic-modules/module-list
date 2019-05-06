ea64bccd8221c06236d5b4ad312c78538503fcb9
Jascha <jascha@jaeh.at>
2019-05-06 17:18:29 +0200

    remove id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const ModuleList = props =>
   CHECK_PROPS(props, ModuleList.props, 'ModuleList') &&
   GitList({
-    id: 'magic-module',
     class: 'ModuleList',
     org: 'magic-modules',
     header: [Link({ to: 'https://magic-modules/github.io' }, '@magic-modules')],
