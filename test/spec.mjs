d3acc55bef100a1a8029f8d6724ea07ca6acdc02
Jascha <jascha@jaeh.at>
2019-12-26 03:36:22 +0100

    update tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -3,14 +3,4 @@ import * as ModuleList from '../src/index.mjs'
 
 export default [
   { fn: () => ModuleList.View, expect: is.function, info: 'expect ModuleList to be a function' },
-  {
-    fn: () => ModuleList.propTypes,
-    expect: is.object,
-    info: 'expect ModuleList to have propTypes object',
-  },
-  {
-    fn: () => ModuleList.propTypes.ModuleList,
-    expect: is.array,
-    info: 'expect ModuleList to have propTypes.ModuleList array',
-  },
 ]
